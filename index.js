05ef1bff0d05cae6444ebc910616ba22287b5373
Michelle Scott <scottmm374@gmail.com>
2019-12-04 18:04:58 -0500

    Get and Post working

--- index.js
+++ index.js
@@ -2,18 +2,66 @@
 
 const express = require("express");
 
-const db = require("./data/db");
+const db = require("./data/db.js");
 
-const server = express();
-server.use(express.json());
+const port = 5000;
+const host = "127.0.0.1";
+const app = express();
+app.use(express.json());
 
-server.get("/", (req, res) => {
-  res.json({ message: "You made it!" });
+app.get("/", (req, res) => {
+  res.send({ message: "You made it!" });
 });
 
-const port = 8080;
-const host = "127.0.0.1";
+// get all users
 
-server.listen(port, host, () => {
+app.get("/api/users", (req, res) => {
+  db.find()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Info not available" });
+    });
+});
+
+// post new user
+
+app.post("/api/users", (req, res) => {
+  //! Need to add if error when saving to this still.
+  if (!req.body.name || !req.body.bio) {
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+  const newUser = {
+    name: req.body.name,
+    bio: req.body.bio
+  };
+
+  db.insert(newUser);
+  res.status(201).json("New user Created");
+});
+
+// delete user
+
+app.delete("api/users/:id", (req, res) => {
+  const user = req.params.id;
+  db.remove()
+    .then(user => {
+      if (!user) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      } else {
+        res.status(200).json("User Deleted");
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: "The user could not be removed" });
+    });
+});
+
+app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);
 });
