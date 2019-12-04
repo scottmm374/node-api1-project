e634a4992a12771e7bdc6ad0712fce743e76e255
Michelle Scott <scottmm374@gmail.com>
2019-12-04 09:14:27 -0500

    Initial server set up and running

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // implement your API here
+
+const express = require("express");
+
+const db = require("./data/db");
+
+const server = express();
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.json({ message: "You made it!" });
+});
+
+const port = 8080;
+const host = "127.0.0.1";
+
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
+});
